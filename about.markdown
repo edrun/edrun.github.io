706ecc83592c9ba763f71f8411f27045635e8c1b
qledrun <qledrun@gmail.com>
2013-09-13 19:03:04 +0800

    my site

---
+++ about.markdown
@@ -0,0 +1,74 @@
+---
+title: about
+layout: page
+---
+
+@me
+
+I am here.
+>[github](http://github.com/edrun)
+or
+>[+1](https://plus.google.com/u/0/116057609163918027040/posts)
+or follow me
+>[@佛如马蹄欧诺](http://weibo.com/1888855315/profile?from=profile&wvr=5&loc=tabprofile#profile_tab)
+
+a
+>
+	$tr -s "qledrun" "Davis" <nick.name  >my.name
+
+b
+>
+    $find /root -maxdepth 1 -type f -path "school" -print 2> /dev/null
+    /root/school/桂林电子科技大学/信息与通信学院/信息对抗技术专业
+
+o
+>
+	$echo "|思考|代码|K歌|吉他|小七|"|tee nowdays
+
+u
+>      
+	$cat  work.txt|grep "FAE"
+   >[GreatVision Co.,Ltd.](http://www.gvtv.com.cn)
+>
+	$cat  work.txt|grep "Software Engineer."
+   >[Realtek Co.,Ltd](http://www.realtek.com.tw)
+
+
+t
+>
+		email.
+		$echo "dyrqeha@tznvy.pbz" | python -c 'import sys; print sys.stdin.read().encode("rot13")'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ gitbash.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo 
+echo 
+echo  "----------------------"
+echo  "Wirtten  for git client."
+echo  "Modify by ql  Ver1.0"
+echo  "----------------------"
+
+COMMAND=""
+while [ "$COMMAND" != "q" ]
+do
+	echo 
+    echo 'Pls be careful to use'
+	echo '---------------------'
+	echo "Select a command:"
+	echo "s. status"
+	echo "a. add -Av"
+	echo "c. commit -avm"
+	echo "p. push -v"
+	echo "gh. push gh-pages -v"
+	echo "u. pull -v"
+	echo "v. archive"
+    echo "sw. show change"
+	echo "q. exit"
+	echo '---------------------'
+	read -r -s -n 1 COMMAND
+	echo ''
+    if [ "$COMMAND" = "s" ] ; then
+		echo ">> git status"
+    	git status
+    elif [ "$COMMAND" = "a" ] ; then
+		echo ">> git add -Av"
+    	git add -Av
+    elif [ "$COMMAND" = "c" ] ; then
+    	echo ">> Please input a message: "
+		read MESSAGE
+		echo ">> git commit -avm \"$MESSAGE\""
+		git commit -avm "$MESSAGE"
+    elif [ "$COMMAND" = "p" ] ; then
+		echo ">> git push -v"
+    	git push origin master
+    elif [ "$COMMAND" = "gh" ] ; then
+		echo ">> git push hg-pages -v"
+    	git push origin gh-pages
+    elif [ "$COMMAND" = "sw" ] ; then
+		echo ">> git show"
+    	git show
+    elif [ "$COMMAND" = "u" ] ; then
+		echo ">> git pull -v"
+    	git pull -v
+	elif [ "$COMMAND" = "v" ] ; then
+		echo ">> git archive master | bzip2 > source-tree.tar.bz2"
+		git archive master | bzip2 > ../source-tree.tar.bz2
+	elif [ "$COMMAND" = "q" ] ; then
+	    exit 0
+	else
+		continue
+	fi
+done
+
+
